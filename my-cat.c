0700eb3aa38bfa6b7bc9ae1cf7e5539c2bc5d0b4
sande <santeri.ruuskanen@student.lut.fi>
2021-04-06 00:28:07 +0300

    first commit

---
+++ my-cat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void read_file(int argc, int i, char filename[40]){
+    char line[200];
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (fp==NULL){
+        printf("my-cat: cannot open file %s\n", filename);
+        //printf("Error when trying to open '%s' -file\n", filename);
+        exit(0);
+    }
+    
+    for(;;){
+        if (fgets(line,150, fp) == NULL){
+            break;
+        }
+        printf("%s", line);
+    }
+    printf("\n");
+    fclose(fp);
+}
+
+
+int main(int argc, char *argv[]){
+    //FILE *fp;
+    //strcpy(filename, argv[1]);
+
+    //fp = fopen(argv[1], "r");
+    
+    if (argc==1){
+    	printf("ANNA NIIT FILUI BOI!!!\n");
+    	return 0;
+    }
+
+    char filename[40];
+
+    for(int i = 1; i < argc; i++){
+        strcpy(filename, argv[i]);
+        printf("%d. input file contents: \n\n", i);
+        read_file(i, argc, filename);
+            
+    }
+    }
+
