044f12e06ecf461523c3b5c71087665f7ba140df
sande <santeri.ruuskanen@student.lut.fi>
2021-04-07 13:50:09 +0300

    terve

--- my-zip/my-zip.c
+++ my-zip/my-zip.c
@@ -3,6 +3,32 @@
 #include <string.h>
 #define MAX_RLEN 50
 
+
+
+
+
+
+
+
+void read_file(char filename[40]){
+	
+	
+
+
+}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
 /* Returns the Run Length Encoded string for the
 source string src */
 char* encode(char* src)
@@ -22,15 +48,14 @@ char* encode(char* src)
 	/* traverse the input string one by one */
 	for (i = 0; i < len; i++) {
 
-		/* Copy the first occurrence of the new character */
-		dest[j++] = src[i];
-
 		/* Count the number of occurrences of the new character */
 		rLen = 1;
 		while (i + 1 < len && src[i] == src[i + 1]) {
 			rLen++;
 			i++;
 		}
+		/* Copy the first occurrence of the new character */
+		dest[j++] = src[i];
 
 		/* Store rLen in a character array count[] */
 		sprintf(count, "%d", rLen);
@@ -47,11 +72,33 @@ char* encode(char* src)
 }
 
 /*driver program to test above function */
-int main()
+int main(int argc, char *argv[])
 {
+
+	char inputfile[40];
+	char outputfile[40];
+	
+	if (argc != 3){
+		//perror("Invalid number of arguments\n");
+		fprintf(stderr,  "my-zip: <file1 file2\n");
+		exit(1);
+	}
+	
+	strcpy(outputfile, argv[argc-1]);
+	
+	for (int i = 1; i < argc-1; i++){
+		strcpy(inputfile, argv[i]);
+		//char* res = encode(str);
+		printf("%s", res);
+	}
+	
 	char str[] = "geeksforgeeks";
 	char* res = encode(str);
-	printf("%s", res);
-	getchar();
+	
+	
+	printf("\n");
+	
+	
+	return 0;
 }
 
