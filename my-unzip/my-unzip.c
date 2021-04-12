9d436b00da8ed98261c87a8699ab90df5ecd4d50
Santeri Ruuskanen <santti.ruuskanen@gmail.com>
2021-04-12 04:58:55 -0700

    unzip

--- my-unzip/my-unzip.c
+++ my-unzip/my-unzip.c
@@ -9,3 +9,37 @@
 
 #include <stdio.h>
 
+
+
+
+int main(int argc, char *argv[]){
+
+
+	
+	if (argc < 3
+	char filename[40];
+	strcpy(filename, argv[1]);
+	
+	FILE *fp = fopen(filename, "r");
+	if(fp == NULL) {
+		perror("Unable to open file!");
+		exit(0);
+	
+	for(;;){
+		fprintf(stdout, "%s", ?)
+	}
+	
+	
+
+
+
+
+
+	}
+
+
+	
+	
+
+
+
